74096e434989e8f038291902b43b7707ef0cd1da
Romain Reghem <romain.reghem@etud.univ-jfc.fr>
2022-09-14 14:22:29 +0200

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import { Text } from '@chakra-ui/react'
+
+function App() {
+  const [count, setCount] = useState(0)
+  return (
+    <Text>Windoge</Text>
+  )
+}
+
+export default App
