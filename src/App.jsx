dcd60b8cebbe1a225ec1e7f32c118e343fa92db3
LeandreGarriga <leandre.garriga@etud.univ-jfc.fr>
2022-09-15 11:47:43 +0200

    Ajout formulaire patient

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import Header from "./Header";
 import Patients from "./Patients";
 import Planning from "./Planning";
 import Messages from "./Messages";
+import FormulairePatient from './FormulairePatient'
 
 function App() {
   return (
@@ -18,6 +19,7 @@ function App() {
         <Route path='/patients' element={<Patients/>} />
         <Route path='/planning' element={<Planning/>} />
         <Route path='/messages' element={<Messages/>} />
+        <Route path='/formulairepatient' element={<FormulairePatient/>}/>
       </Routes>
     </>
   )
---
+++ src/FormulairePatient.jsx
@@ -0,0 +1,11 @@
+import { Text, Center, Badge, Stack, InputLeftElement, InputGroup, Input, Button, Spinner } from "@chakra-ui/react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faMagnifyingGlass, faPlus } from '@fortawesome/free-solid-svg-icons'
+
+const FormulairePatient = ()  =>{
+    return(
+        <Text>Formulaire</Text>
+    )
+}
+
+export default FormulairePatient;
