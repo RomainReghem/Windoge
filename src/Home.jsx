f77413997845b6d66c3702d3f04e8bdf5e80e87d
imane_aoubiza <imane.aoubiza@etud.univ-jfc.fr>
2022-09-15 11:51:23 +0200

    home page

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,13 +1,43 @@
-import { Center, Heading, Text, Stack } from "@chakra-ui/react";
+import { Center, Heading, Text, Stack, Input, Button, FormLabel, useToast } from "@chakra-ui/react";
+import { Link, useNavigate } from "react-router-dom";
+
 
 const Home = () => {
+    const toast = useToast()
+    const navigate = useNavigate()
     return (
-    <Center h={'100%'}>
-        <Stack align={'center'}>
-        <Heading>Salut 😹 </Heading>
-        <Text>Bienvenue</Text>
-        </Stack>    
-    </Center>
+        <>
+            <Center h={'100%'} flexDirection='column' gap='20'>
+                <Stack align={'center'}>
+                    <Heading> Welcome to Windoge 5, your trusty link with your patients </Heading>
+                </Stack>
+                <Stack align={'center'} flexDirection='column' gap='5'>
+                    <FormLabel> Username
+                        <Input variant='filled' placeholder='Enter your username'></Input>
+                    </FormLabel>
+                    <FormLabel> Password
+                        <Input variant='filled' placeholder='Enter your password'></Input>
+                    </FormLabel>
+                </Stack>
+                <Stack>
+                    <Button
+                        onClick={() => {
+                            toast({
+                                title: 'You have successfully logged in',
+                                description: "Welcome dear practician",
+                                status: 'success',
+                                duration: 9000,
+                                position: 'top-right',
+                                isClosable: true,
+                            });
+                            navigate('/patients')
+                        }}
+                    >
+                        Log In
+                    </Button>
+                </Stack>
+            </Center>
+        </>
     )
 }
 
