e78a4012e68c0d3ee3cbe3087c9645e51d3f24f1
Romain Reghem <romain.reghem@etud.univ-jfc.fr>
2022-09-21 14:47:28 +0200

    ref medecin

--- src/Destinataire.jsx
+++ src/Destinataire.jsx
@@ -3,10 +3,10 @@ import { useEffect, useState } from "react";
 
 const Destinataire = (props) => {
     const [patients, setPatients] = useState(null);
-    const [selected, setSelected] = useState(667)
+    const [selected, setSelected] = useState(0)
 
     useEffect(() => {
-        fetch(' https://fhir.alliance4u.io/api/Patient?generalPractitioner.reference=6321e0f8d83022001917f14b')
+        fetch(' https://fhir.alliance4u.io/api/Patient?generalPractitioner.reference=Practitioner/7')
             .then(response => response.json())
             .then(data => setPatients(data));
     }, [])
@@ -19,9 +19,6 @@ const Destinataire = (props) => {
         <Stack w={'2xs'} borderRight={'1px solid'} borderColor='gray.300' p={'4'} overflow='scroll'>
             <Heading fontSize={'xl'}>Your patients</Heading>
             <Divider></Divider>
-            <Button onClick={() => { handleSelectPatient("62f65031c87c9100196ec0a5"); setSelected(667) }} key={667} p='4' variant={667 == selected ? 'solid' : 'outline'} colorScheme={'gray'} cursor={'pointer'}>
-                <Text>Monsieur Exemple</Text>
-            </Button>
             {
                 patients?.map((element, index) => {
                     if (element.name) {
